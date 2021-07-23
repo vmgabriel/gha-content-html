cd802474395a6523a0de1f5a3290960febcad608
Gabriel Vargas <vmgabrielf96@gmail.com>
2021-07-23 17:18:37 -0500

    feat: control of main generated

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 """Main Application"""
 
 import markdown
+import jinja2
 from actions_toolkit import core
-from jinja2 import Template
 
 
 md = markdown.Markdown(extensions=['meta'])
@@ -11,10 +11,14 @@ template = core.get_input('template', required=True)
 body = core.get_input('body', required=True)
 
 template_content = open(template, "r").read()
-template = Template(template_content)
 
-email_info = template.render(**{
-    "content": md.convert(body),
+env = jinja2.Environment()
+env.filters['markdown'] = lambda text: jinja2.Markup(md.convert(text))
+env.trim_blocks = True
+env.lstrip_blocks = True
+
+email_info = env.from_string(template_content).render(**{
+    "content": body,
 })
 
 core.info(f"Content body - { body }")
