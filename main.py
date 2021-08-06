d43b6a8b854b144ad0ab1c2b4d15065f65d40c3d
= <vmgabriel96@gmail.com>
2021-08-06 12:38:04 -0500

    feat: content of gha converted to json

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
 """Main Application"""
 
-from markdown2 import Markdown
 import jinja2
+import json
+from typing import List
+from markdown2 import Markdown
 from actions_toolkit import core
 
 
@@ -10,9 +12,8 @@ md = Markdown()
 
 template = core.get_input('template', required=True)
 body = core.get_input('body', required=True)
-# body = "changelog->;uncategorized->title|fix: control module status~number|1.0~url|https://github.com/vmgabriel/test-semantic-version-ga/pull/50~mergedat|2021-07-26T15:40:52.000Z~author|vmgabriel~body|content data °title|fix: control module status~number|1.0~url|https://github.com/vmgabriel/test-semantic-version-ga/pull/50~mergedat|2021-07-26T15:40:52.000Z~author|vmgabriel~body|content data ;ignored->;repo->test-semantic-version-ga;fromtag->2.0.5;totag->2.1.0;categorizedcount->0;uncategorizedcount->1;ignoredcount->0"
+
 template_content = open(template, "r").read()
-# template_content = "<html><body>{{ content|safe }}</body></html>"
 
 # Convert markdown to html
 env = jinja2.Environment()
@@ -24,22 +25,41 @@ email_info = env.from_string(template_content).render(**{
     "content": body,
 })
 
+
+def convert_to_list(content_str: str) -> List[dict]:
+    prs = content_str.split("^")[:-1]
+    content_prs = []
+    for pr in prs:
+        content_pr = {}
+        pr_datas = pr.split("~")
+        for pr_data in pr_datas:
+            min_data = pr_data.split("|")
+            content_pr[min_data[0]] = min_data[1]
+        content_prs.append(content_pr)
+    return content_prs
+
+
 dict_content = {}
 main_content = body.split(";")
 INTERN_CONTENTS = ["changelog", "uncategorized", "ignored"]
 for content in main_content:
     data_content = content.split("->")[1]
-    if len(data_content.split("~")) > 1:
-        intern_data_content = data_content.split("~")
-        for s_data_content in intern_data_content:
-            n_data_content = {}
-            s_s_data_content = s_data_content.split("|")
-            n_data_content[s_s_data_content[0]] = s_s_data_content[1]
-        data_content = n_data_content
-    dict_content[content.split("->")[0]] = data_content
 
-    if content.split("->")[0] in INTERN_CONTENTS and data_content == "":
-        dict_content[content.split("->")[0]] = {}
+    changes_content = data_content.split("\n\n")
+    if len(changes_content) > 1:
+        data_content = changes_content[:-1]
+
+        state_content = {}
+        for cont_pos in range(len(data_content) // 2):
+            state_content[data_content[2 * cont_pos]] = convert_to_list(
+                data_content[(2 * cont_pos) + 1]
+            )
+        data_content = state_content
+
+    if content.split("->")[0] in INTERN_CONTENTS[1:]:
+        data_content = convert_to_list(data_content)
+
+    dict_content[content.split("->")[0]] = data_content
 main_content = dict_content
 
 email_info = env.from_string(template_content).render(**{
@@ -51,4 +71,3 @@ core.info(f"Content body - { body }")
 core.info(f"Email body - {email_info}")
 
 core.set_output('html_data', email_info)
-# print("Email info - ", main_content)
