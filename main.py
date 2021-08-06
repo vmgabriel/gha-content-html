322058db9187a540ed3c6cb1a271d9323fa384e8
= <vmgabriel96@gmail.com>
2021-08-06 15:46:27 -0500

    feat: change content split

--- main.py
+++ main.py
@@ -27,25 +27,25 @@ email_info = env.from_string(template_content).render(**{
 
 
 def convert_to_list(content_str: str) -> List[dict]:
-    prs = content_str.split("^")[:-1]
+    prs = content_str.split("(^)")[:-1]
     content_prs = []
     for pr in prs:
         content_pr = {}
-        pr_datas = pr.split("~")
+        pr_datas = pr.split("(~)")
         for pr_data in pr_datas:
-            min_data = pr_data.split("|")
+            min_data = pr_data.split("(|)")
             content_pr[min_data[0]] = min_data[1]
         content_prs.append(content_pr)
     return content_prs
 
 
 dict_content = {}
-main_content = body.split(";")
+main_content = body.split("(;)")
 INTERN_CONTENTS = ["changelog", "uncategorized", "ignored"]
 for content in main_content:
-    data_content = content.split("->")[1]
+    data_content = content.split("(->)")[1]
 
-    changes_content = data_content.split("\n\n")
+    changes_content = data_content.split("(°)\n\n")
     if len(changes_content) > 1:
         data_content = changes_content[:-1]
 
@@ -56,10 +56,10 @@ for content in main_content:
             )
         data_content = state_content
 
-    if content.split("->")[0] in INTERN_CONTENTS[1:]:
+    if content.split("(->)")[0] in INTERN_CONTENTS[1:]:
         data_content = convert_to_list(data_content)
 
-    dict_content[content.split("->")[0]] = data_content
+    dict_content[content.split("(->)")[0]] = data_content
 main_content = dict_content
 
 email_info = env.from_string(template_content).render(**{
